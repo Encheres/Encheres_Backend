7829e2f8c1075304450e9397c9cd86b385194c3e
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-09-16 13:39:56 +0530

    resolved mongodb issues

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,8 +3,14 @@ mongoose.connect(
   `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.tdun5.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+    useUnifiedTopology: true
   }
 );
+
+mongoose.connection.on("error", function(error) {
+  console.log(error)
+})
+
+mongoose.connection.on("open", function() {
+  console.log("Connected to MongoDB database.")
+})
