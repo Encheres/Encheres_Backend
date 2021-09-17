ec67f443ec07924a44b6929450fdd838fc159c5e
bhavesh <bkpassi2017@gmail.com>
2021-09-18 00:44:27 +0530

    Auction Model and Routes added

---
+++ models/auction.js
@@ -0,0 +1,89 @@
+
+const mongoose = require("mongoose");
+
+const auctionSchema = new mongoose.Schema(
+    {
+        tags: {
+            type: [String],
+            required: true
+        },
+        event_date_time: {
+            type: Date,
+            required: true,
+            default: Date.now
+        },
+        pickup_point: {
+            addressLine1:{
+                type:String,
+                trim: true,
+                required: true,
+            },
+            addressLine2:{
+                type:String,
+                trim: true
+            },
+            city:{
+                type:Object,
+                trim: true,
+                required: true,
+            },
+            state:{
+                type:String,
+                trim: true,
+                required: true,
+            },
+            postalCode:{
+                type:String,
+                trim: true,
+                required: true
+            },
+            country:{
+                type:String,
+                trim: true,
+                required:true
+            }
+        },
+        organizer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        completed: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        organizer_contact: {
+            type: Number,
+            required: true
+        },
+        items: [{
+            name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        quantity: {
+            type: String,
+            required: true,
+            default: 1
+        },
+        aggregate_base_price: {
+            type: Number,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        images: [{
+            type: String,
+            required: true
+        }],
+        video: {
+            type: String
+        }}]
+    }, {timestamps: true}
+);
+
+const Auction = mongoose.model("Auction", auctionSchema);
+module.exports = Auction;
---
+++ routes/auctions.js
@@ -0,0 +1,94 @@
+var express = require('express');
+var router = express.Router();
+const Auction = require('../models/auction');
+
+// For tag-filtered query.
+router.route('/filtered-auctions')
+.get(async (req, res, next) => {
+
+    const PAGE_SIZE = 5;
+    const page = parseInt(req.query.page || "0");
+
+    try{
+        var auctions = await Auction.find({"tags" : { $in: req.query.tags.split(',')}})
+        .sort({'event_date_time':1}).limit(PAGE_SIZE).skip(PAGE_SIZE*page);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(auctions);
+    }
+    catch (error) {
+        next(error);
+    }
+})
+
+// For Usual auction listing.
+router.route('/auctions')
+.get(async (req, res, next) => {
+
+    const PAGE_SIZE = 5;
+    const page = parseInt(req.query.page || "0");
+
+    try{
+        var auctions = await Auction.find({})
+        .sort({'event_date_time':1}).limit(PAGE_SIZE).skip(PAGE_SIZE*page);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(auctions);
+    }
+    catch (error) {
+        next(error);
+    }
+})
+.post(async (req, res, next) => {
+    try {
+        var auction = new Auction(req.body);
+        await auction.save();
+        res.status(200).json(auction);
+    } catch (error) {
+        next(error);
+    }
+})
+
+// For particular auction CRUD.
+router.route('/auctions/:auctionId')
+.get(async (req, res, next) => {
+    try {
+        var auction = await Auction.findById(req.params.auctionId);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(auction);
+
+    } catch (error) {
+        next(error);
+    }
+})
+.put(async (req, res, next) => {
+
+    try {
+        var auction = await Auction.findByIdAndUpdate(req.params.auctionId, 
+            {$set: req.body}, 
+            {new: true});
+        
+        console.log('Sucess!!');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(auction);
+
+    } catch (error) {
+        next(error);
+    }
+})
+.delete(async (req, res, next) => {
+
+    try {
+		var operation = await Auction.deleteOne({"_id": req.params.auctionId});
+		res.status(200).json(operation);
+	} catch (error) {
+		next(error);
+	}
+
+})
+
+module.exports = router;
