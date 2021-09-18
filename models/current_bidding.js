f515ba3ed95937435a629cb1793c895b325796a3
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-09-19 00:34:48 +0530

    Added Current Bidding Schema

---
+++ models/current_bidding.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const currentBiddingSchema = new mongoose.Schema({
+    // The current bidding
+    auction_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Auction',
+        required: true
+    },
+    item_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Auction',
+        required: true
+    },
+    bidder:{
+        user_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        },
+        anonymous_name:{
+            type: String,
+            required: true
+        },
+    },
+    current_price:{
+        type: Double,
+        required: true
+    }
+}, {timeStamp: true});
+
+
+const CurrentBidding = mongoose.model("CurrentBidding", currentBiddingSchema);
+module.exports = CurrentBidding;
