3912eeccfe5d873b1726b43ecca1b1e540db1fad
kaw <tejvirus0003@gmail.com>
2021-09-18 11:00:04 +0530

    contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -5,8 +5,7 @@ const schema = {
     type: String,
     trim: true,
     unique: true,
-    required: "Email address is required",
-    validate: [validateEmail, "Please fill a valid email address"],
+    required: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       "Please fill a valid email address",
