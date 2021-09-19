3b7119115f06506897f511762c6a8092dc5afe7c
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-09-19 16:01:01 +0530

    Added authentication system

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,23 @@
 var express = require('express');
 const User = require("../models/user");
 var router = express.Router();
+const auth = require('../middleware/auth');
+const jwt = require('jsonwebtoken');
+const {sendPasswordResetEmail} = require('../middleware/emails');
 
-/* GET users listing. */
+/* GET users listing.           --- To be deleted*/
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post("/users", async function(req, res, next) {
-  console.log("Hi");
+
+/*-----------------------------------------------------*/
+/* AUTHENTICATION RELATED */
+/*-----------------------------------------------------*/
+
+// Add a new user/ Signup
+router.post("/signup", async function(req, res, next) {
 	const user = new User(req.body);
-	// console.log(req.body);
 	try {
 		await user.save();
 		const token = await user.generateAuthToken();
@@ -21,7 +28,34 @@ router.post("/users", async function(req, res, next) {
 	}
 });
 
+// Login
+router.post('/login',async(req,res)=>{
+    try{
+        const user = await User.findByCredentials(req.body.email,req.body.password);
+        const token = await user.generateAuthToken()//Generate jwt authentication token
+        res.send({user,token})
+    }
+    catch(e){
         res.status(400).send('Details Mismatched');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    }
+})
+
+//Logout
+router.post('/logout',auth, async(req,res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((token)=>{
+            return token.token!== req.token
+        })
+        await req.user.save()
+        res.send()
+    }
+    catch(e){
+        res.status(500).send(e);
+    }
+});
+
+
+	
 
 
 module.exports = router;
