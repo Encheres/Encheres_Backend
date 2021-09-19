1954960513866265ae03c287dbf311a4233e07d8
tejvirus0003 <tejvirus0003@gmail.com>
2021-09-19 17:42:44 +0530

    contactus router

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var auctionsRouter = require('./routes/auctions');
 var orderRouter = require('./routes/orders');
-
+var ContactUsRouter = require('./routes/contactus')
 var app = express();
 
 // view engine setup
@@ -28,22 +28,22 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(auctionsRouter);
 app.use(orderRouter);
-
+app.use('/contact-us', ContactUsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
