8af749454b8863cb037eee5bfc575220b1fb6a78
bhavesh <bkpassi2017@gmail.com>
2021-11-14 14:07:52 +0530

    Enabled CORS options for FastApi

--- generative_models/fastApiApp/main.py
+++ generative_models/fastApiApp/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from literature_generator_script import *
 
 
 app = FastAPI() # Instantiating FastApi App Object.
 
+# Enabling CORS options.
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Defining Api end-points
 
