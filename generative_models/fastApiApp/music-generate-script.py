dda6c57faa1da11181b409686f7e97b2f3118323
tejvirus0003 <tejvirus0003@gmail.com>
2021-11-22 16:13:17 +0530

    added model music

---
+++ generative_models/fastApiApp/music-generate-script.py
@@ -0,0 +1,64 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+import pickle
+
+def chords_n_notes(Snippet):
+    Melody = []
+    offset = 0 #Incremental
+    for i in Snippet:
+        #If it is chord
+        if ("." in i or i.isdigit()):
+            chord_notes = i.split(".") #Seperating the notes in chord
+            notes = [] 
+            for j in chord_notes:
+                inst_note=int(j)
+                note_snip = note.Note(inst_note)            
+                notes.append(note_snip)
+                chord_snip = chord.Chord(notes)
+                chord_snip.offset = offset
+                Melody.append(chord_snip)
+        # pattern is a note
+        else: 
+            note_snip = note.Note(i)
+            note_snip.offset = offset
+            Melody.append(note_snip)
+        # increase offset each iteration so that notes do not stack
+        offset += 1
+    Melody_midi = stream.Stream(Melody)   
+    return Melody_midi
+
+
+def Malody_Generator(Note_Count):
+    reverse_mapping = dict()
+    with open('./symb.txt','rb') as fp:
+        b=pickle.load(fp)
+        reverse_mapping = dict((i, c) for i, c in enumerate(b))
+    
+    model = load_model('./music-generator.h5', compile=False)
+    length=40
+    X_seed = np.load('./Xarr.npy')
+    seed = X_seed[np.random.randint(0,len(X_seed)-1)]
+    Music = ""
+    Notes_Generated=[]
+    for i in range(Note_Count):
+        seed = seed.reshape(1,length,1)
+        prediction = model.predict(seed, verbose=0)[0]
+        prediction = np.log(prediction) / 1.0 #diversity
+        exp_preds = np.exp(prediction)
+        prediction = exp_preds / np.sum(exp_preds)
+        index = np.argmax(prediction)
+        index_N = index/ float(53712)   
+        Notes_Generated.append(index)
+        Music = [reverse_mapping[char] for char in Notes_Generated]
+        seed = np.insert(seed[0],len(seed[0]),index_N)
+        seed = seed[1:]
+    #Now, we have music in form or a list of chords and notes and we want to be a midi file.
+    Melody = chords_n_notes(Music)
+    Melody_midi = stream.Stream(Melody)   
+    
+    return Music,Melody_midi
+    
+    #return 0
+print('done')
+Music_notes, Melody = Malody_Generator(100)
+show(Melody)
